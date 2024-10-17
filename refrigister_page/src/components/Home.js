a25a2c177f90fe552c2e3b6adfc74884462d03ad
F-ritsu-ritsu <ritsuki.fk@gmail.com>
2024-10-17 22:43:39 +0900

    add,Listコンポーネントの追加

---
+++ refrigister_page/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import List from "./List";
+
+const Home = () => {
+  return (
+    <div>
+      <List/>
+    </div>
+  );
+};
+
+export default Home;
---
+++ refrigister_page/src/components/List.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const List = () => {
+  return (
+    <div>
+      <h1>aaa</h1>
+    </div>
+  );
+};
+
+export default List;
